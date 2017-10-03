14789d0af55861439a9df65f79f7ff4d815ce215
Leon Lee <leon.lee@berkeley.edu>
2017-10-03 06:43:04 -0700

    multiple routes for index page.

--- webarc/main/views.py
+++ webarc/main/views.py
@@ -5,7 +5,7 @@ from webarc import app
 
 main_blueprint = Blueprint('main', __name__,)
 
-
+@main_blueprint.route('/')
 @main_blueprint.route('/index')
 def index():
     return render_template('index.html')
